38a751fc6adca878997278583eaa1f8c7b2e73ad
Ramin <ek3nk4r@gmail.com>
2022-02-01 00:25:05 +0330

    Update deploy.ts

--- pages/api/tezos/deploy.ts
+++ pages/api/tezos/deploy.ts
@@ -25,7 +25,7 @@ export default async function deploy(
 
     res.status(200).json({
       contractAddress: contract.address,
-      hash: operation.hash
+      hash: operation.hash,
     });
   } catch (error) {
     let errorMessage = error instanceof Error ? error.message : 'Unknown Error';
